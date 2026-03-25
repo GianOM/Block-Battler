1b0f67366edb37036c4ffd421fe6361c6a4aafec
GianOM <gianvitor.om@gmail.com>
2026-03-25 05:20:25 -0300

    Map Generator V1

--- Map Generator/Map_Generator__SCRIPT.gd
+++ Map Generator/Map_Generator__SCRIPT.gd
@@ -7,7 +7,8 @@ const EXAMPLE_ROOM__SCENE = preload("uid://ch2t4yt6uxcwt")
 var List_of_Positions_Walked: Array[Vector2i]
 
 
-@export var number_of_room: int = 64
+@export var number_of_room: int = 40
+@export var starter_position: Vector2i = Vector2i.ZERO
 
 
 var RNG = RandomNumberGenerator.new()
@@ -34,10 +35,9 @@ func Clear_All_Room():
 		individual_room.queue_free()
 	
 func Drunkard_Walk():
+	
 	var Remaining_Number_of_Placed_Rooms: int = number_of_room
-	
-	
-	var current_drunk_position : Vector2i = Vector2i(0,0)
+	var current_drunk_position : Vector2i = starter_position
 	
 	
 	
@@ -46,9 +46,7 @@ func Drunkard_Walk():
 		if not List_of_Positions_Walked.has(current_drunk_position):
 			
 			List_of_Positions_Walked.push_back(current_drunk_position)
-			
 			Instance_Room_on_drunk_position(current_drunk_position)
-			
 			Remaining_Number_of_Placed_Rooms -= 1
 			
 		current_drunk_position = Take_Drunken_Step(current_drunk_position)
@@ -60,7 +58,7 @@ func Instance_Room_on_drunk_position(input_drunk_position: Vector2i):
 	
 	
 	
-	var Global_XYZ_Room_Coordinates: Vector3 = (Vector3(input_drunk_position.x,0,input_drunk_position.y))*4
+	var Global_XYZ_Room_Coordinates: Vector3 = (Vector3(input_drunk_position.x,0,input_drunk_position.y)) * 4
 	
 	var Temp_Scene: Node = EXAMPLE_ROOM__SCENE.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
 	
@@ -93,6 +91,14 @@ func Take_Drunken_Step(input_position: Vector2i) -> Vector2i:
 		3:#RIGHT STEP
 			drunk_position_after_step += input_position - Vector2i(0,1)
 			
+	if (drunk_position_after_step.distance_to(starter_position)) > (float(number_of_room) / 4.0):
+		
+		var next_starter_positon_idx: int = RNG.randi_range(0, List_of_Positions_Walked.size() - 1)
+		
+		starter_position = List_of_Positions_Walked[next_starter_positon_idx]
+		
+		drunk_position_after_step = starter_position
+		print("VOLTE SUA MISEERA")
 	
 	return drunk_position_after_step
 	
